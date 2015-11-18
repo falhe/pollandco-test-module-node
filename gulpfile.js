1e3c5c471c249e0cae967d2da3b1ddb79c140863
Falihery Rasolonirainy <falihery.rasolonirainy@gmail.com>
2015-11-18 23:37:06 +0100

    MAJ package.json et [TO DO ]gulpfile
    Utilisation de gulp à la place de nom directement, plus facile à maintenir
    
    Signed-off-by: Falihery Rasolonirainy <falihery.rasolonirainy@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp            = require('gulp');
+/*var gulp            = require('gulp');
 var browserify      = require('gulp-browserify');
 var watchify        = require('watchify');
 var uglify          = require('gulp-uglify');
@@ -6,8 +6,95 @@ var hbsfy           = require("hbsfy");
 var sass            = require('gulp-sass');
 var source          = require('vinyl-source-stream');
 
-var elixir          = require('laravel-elixir');
-//var browserify = require('laravel-elixir-browserify');
+var elixir          = require('laravel-elixir'); */
+
+
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync').create(),
+    watchify = require('watchify'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'), //Convert streaming vinyl to buffer for uglify
+    bundler = watchify(browserify('./app/app.js'), {debug: true}),
+    mocha = require('gulp-mocha');
+
+// TO DO BIEN FAIRE LE FICHIER GULP pour les watch sass et js
+
+bundler.on('update', bundle);
+
+function bundle() {
+    gutil.log('Compiling JS...');
+    return bundler.bundle().on('error', function(err) {
+            gutil.log(err.message);
+            browserSync.notify('Browserify Error!');
+            this.emit('end');
+        })
+        .pipe(source('bundle.js'))
+        // .pipe(buffer())
+        // .pipe(uglify())
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.stream({
+            once: true
+        }));
+}
+
+gulp.task('bundle', function() {
+    return bundle();
+});
+
+gulp.task('styles', function() {
+    return gulp.src('./scss/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['styles', 'bundle'], function() {
+    browserSync.init({
+        server: './'
+    });
+    gulp.watch('./scss/*.scss', ['styles']);
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('./app/views/templates/*.hbs').on('change', browserSync.reload);
+});
+
+gulp.task('test', function() {
+    return gulp.src('./test/*.js', {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha());
+});
+
+
+// gulp.task('browserify', function() {
+//     return browserify('js/app.js').bundle()
+//     .pipe(source('bundle.js'))
+//     .pipe(gulp.dest('./build/'));
+// });
+
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: './'
+//         }
+//     });
+// });
+
+
+// gulp.task('default', ['styles', 'browser-sync'], function() {
+//     gulp.watch('scss/*.scss', ['styles']);
+//     gulp.watch('js/*.js', ['scripts']);
+//     gulp.watch('*.html').on('change', browserSync.reload);
+// });
+
+//***** ANCIEN GULPFILE A PARTIR D ICI ******//
+
 
 /*
  |--------------------------------------------------------------------------
@@ -21,7 +108,7 @@ var elixir          = require('laravel-elixir');
  */
 
 
-elixir(function(mix) {
+/*elixir(function(mix) {
     // make sure this line is inside of elixir callback function
     // to replace default browserify task
     //browserify.init();
@@ -41,7 +128,7 @@ elixir(function(mix) {
             .pipe(source('main.min.js'))
             .pipe(gulp.dest('public/js/'));
     });
-});
+}); */
 
 /*
 gulp.task('js', function(){
